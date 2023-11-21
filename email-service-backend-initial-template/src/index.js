8821a2da04797dcee9910db6feee644ac3c6c9db
Leonardo Soares <mascoty2005@gmail.com>
2023-11-21 12:27:21 -0300

    refactor: 🦄

--- email-service-backend-initial-template/src/index.js
+++
@@ -1,8 +0,0 @@
-const fastify = require('fastify');
-const server = fastify();
-const emailRoutes = require('./routes/emailRoutes');
-
-server.register(emailRoutes);
-
-server.listen({port: process.env.PORT || 3200});
-
